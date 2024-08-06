04eba7ebc99b3478e31e242515f9f97fe542dab4
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-06 22:15:07 +0530

    feat: added meta files

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { BASE_URL } from "@/constants";
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${BASE_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
